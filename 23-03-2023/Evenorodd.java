b70c25a1397108db02f7d95eb4c5ef5e8a07e36f
Joshma Bandusab Nadaf <128676076+Joshma1@users.noreply.github.com>
2023-03-24 12:35:53 +0530

    Add files via upload

---
+++ 23-03-2023/Evenorodd.java
@@ -0,0 +1,16 @@
+/* write a program to find if the number is odd or even*/
+class Evenorodd
+{
+  public static void main(String args[])
+  {
+    int i=5;
+    if (i%2==0)
+    {  
+      System.out.println("Even no:"+i);
+    }
+    else
+    {
+    System.out.println("Odd no:"+i);
+    }
+   }
+}
