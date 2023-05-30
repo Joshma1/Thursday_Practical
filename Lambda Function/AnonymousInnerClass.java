3501c11d68a398d3b278b0040e4c4b5f900cc88e
Joshma Bandusab Nadaf <128676076+Joshma1@users.noreply.github.com>
2023-05-30 12:18:23 +0530

    Update AnonymousInnerClass.java

--- Lambda Function/AnonymousInnerClass.java
+++ Lambda Function/AnonymousInnerClass.java
@@ -1,19 +1,21 @@
 package lambdaFunction;
-@FunctionalInterface
+@FunctionalInterface //FunctionalInterface
 interface D
 {
 	void display();
 }
-public class AnonymousInnerClass {
+public class AnonymousInnerClass //Main Function With Anonymous Inner Class.
+{
 
-	public static void main(String[] args) {
-		D obj=new  D(){
+	public static void main(String[] args) 
+	{
+		D obj=new  D(){ //object create with Anonymous inner class
 			public void display()
 			{
-				System.out.println("Hi...");
+				System.out.println("Hi..."); //print function
 			}
 		};
-		obj.display();
+		obj.display(); // called display method 
 	}
 
 }
